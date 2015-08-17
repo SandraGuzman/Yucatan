e3944fe846734fa05bba4881656a5fe0b698ee88
SandraGuzman <sandra.guzba18@gmail.com>
2015-08-17 15:28:08 -0500

    Initial Commit

---
+++ Yucatan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Yucatan
+//
+//  Created by Sandra Guzmán Bautista on 17/08/15.
+//  Copyright (c) 2015 onikom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
